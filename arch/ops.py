3e2f03e55f3759df9c41fa9414b75f53e80080d5
LLH00ZZ <71865939+LLH00ZZ@users.noreply.github.com>
2022-08-06 12:03:24 +0800

    Add files via upload

---
+++ arch/ops.py
@@ -0,0 +1,199 @@
+import functools
+from collections import OrderedDict
+
+from torch.nn import init
+import torch.nn as nn
+import torch
+import torch.nn.functional as F
+
+def get_norm_layer(norm_type='instance'):
+    if norm_type == 'batch':
+        norm_layer = functools.partial(nn.BatchNorm2d, affine=True)
+    elif norm_type == 'instance':
+        norm_layer = functools.partial(nn.InstanceNorm2d, affine=False, track_running_stats=False)
+    else:
+        raise NotImplementedError('normalization layer [%s] is not found' % norm_type)
+    return norm_layer
+
+def init_weights(net, init_type='normal', gain=0.02):
+    def init_func(m):
+        classname = m.__class__.__name__
+        if hasattr(m, 'weight') and (classname.find('Conv') != -1 or classname.find('Linear') != -1):
+            init.normal(m.weight.data, 0.0, gain)
+            if hasattr(m, 'bias') and m.bias is not None:
+                init.constant(m.bias.data, 0.0)
+        elif classname.find('BatchNorm2d') != -1:
+            init.normal(m.weight.data, 1.0, gain)
+            init.constant(m.bias.data, 0.0)
+
+    print('Network initialized with weights sampled from N(0,0.02).')
+    net.apply(init_func)
+
+
+def init_network(net, gpu_ids=[]):
+    if len(gpu_ids) > 0:
+        assert(torch.cuda.is_available())
+        net.cuda(gpu_ids[0])
+        net = torch.nn.DataParallel(net, gpu_ids)
+    init_weights(net)
+    return net
+
+
+def conv_norm_lrelu(in_dim, out_dim, kernel_size, stride = 1, padding=0,
+                                 norm_layer = nn.BatchNorm2d, bias = False):
+    return nn.Sequential(
+        nn.Conv2d(in_dim, out_dim, kernel_size, stride, padding, bias = bias),
+        norm_layer(out_dim), nn.LeakyReLU(0.2,True))
+
+def conv_norm_relu(in_dim, out_dim, kernel_size, stride = 1, padding=0,
+                                 norm_layer = nn.BatchNorm2d, bias = False):
+    return nn.Sequential(
+        nn.Conv2d(in_dim, out_dim, kernel_size, stride, padding, bias = bias),
+        norm_layer(out_dim), nn.ReLU(True))
+
+def dconv_norm_relu(in_dim, out_dim, kernel_size, stride = 1, padding=0, output_padding=0,
+                                 norm_layer = nn.BatchNorm2d, bias = False):
+    return nn.Sequential(
+        nn.ConvTranspose2d(in_dim, out_dim, kernel_size, stride,
+                           padding, output_padding, bias = bias),
+        norm_layer(out_dim), nn.ReLU(True))
+def _bn_function_factory(norm, relu, conv):
+    def bn_function(*inputs):
+        concated_features = torch.cat(inputs, 1)
+        bottleneck_output = conv(relu(norm(concated_features)))
+        return bottleneck_output
+
+    return bn_function
+class _DenseLayer(nn.Sequential):
+    """Basic unit of DenseBlock (using bottleneck layer) """
+    def __init__(self, num_input_features, growth_rate, bn_size, drop_rate, efficient=False):
+        super(_DenseLayer, self).__init__()
+        self.add_module('norm1', nn.InstanceNorm2d(num_input_features)),
+        self.add_module('relu1', nn.ReLU(inplace=True)),
+        self.add_module('conv1', nn.Conv2d(num_input_features, bn_size * growth_rate,
+                        kernel_size=1, stride=1, bias=False)),
+        self.add_module('norm2', nn.InstanceNorm2d(bn_size * growth_rate)),
+        self.add_module('relu2', nn.ReLU(inplace=True)),
+        self.add_module('conv2', nn.Conv2d(bn_size * growth_rate, growth_rate,
+                        kernel_size=3, stride=1, padding=1, bias=False)),
+        self.drop_rate = drop_rate
+        self.efficient = efficient
+
+    def forward(self, *prev_features):
+        bn_function = _bn_function_factory(self.norm1, self.relu1, self.conv1)
+        if self.efficient and any(prev_feature.requires_grad for prev_feature in prev_features):
+            bottleneck_output = cp.checkpoint(bn_function, *prev_features)
+        else:
+            bottleneck_output = bn_function(*prev_features)
+        new_features = self.conv2(self.relu2(self.norm2(bottleneck_output)))
+        if self.drop_rate > 0:
+            new_features = F.dropout(new_features, p=self.drop_rate, training=self.training)
+        return new_features
+
+class _DenseBlock(nn.Sequential):
+    """DenseBlock"""
+    def __init__(self, num_layers, num_input_features, bn_size, growth_rate, drop_rate, efficient=False):
+        super(_DenseBlock, self).__init__()
+        for i in range(num_layers):
+            layer = _DenseLayer(
+                num_input_features + i * growth_rate,
+                growth_rate=growth_rate,
+                bn_size=bn_size,
+                drop_rate=drop_rate,
+                efficient=efficient,
+            )
+            self.add_module('denselayer%d' % (i + 1), layer)
+
+    def forward(self, init_features):
+        features = [init_features]
+        for name, layer in self.named_children():
+            new_features = layer(*features)
+            features.append(new_features)
+        return torch.cat(features, 1)    
+class _Transition(nn.Sequential):
+    """Transition layer between two adjacent DenseBlock"""
+    def __init__(self, num_input_feature, num_output_features):
+        super(_Transition, self).__init__()
+        self.add_module("norm", nn.BatchNorm2d(num_input_feature))
+        self.add_module("relu", nn.ReLU(inplace=True))
+        self.add_module("conv", nn.Conv2d(num_input_feature, num_output_features,
+                                          kernel_size=1, stride=1, bias=False))
+        self.add_module("pool", nn.AvgPool2d(2, stride=2))
+
+class DenseNet(nn.Module):
+    #"DenseNet-BC model"
+    def __init__(self, growth_rate=32, block_config=(6, 12, 24, 16), num_init_features=64,
+                 bn_size=4, compression_rate=0.5, drop_rate=0, num_classes=1000):
+        """
+        :param growth_rate: (int) number of filters used in DenseLayer, `k` in the paper
+        :param block_config: (list of 4 ints) number of layers in each DenseBlock
+        :param num_init_features: (int) number of filters in the first Conv2d
+        :param bn_size: (int) the factor using in the bottleneck layer
+        :param compression_rate: (float) the compression rate used in Transition Layer
+        :param drop_rate: (float) the drop rate after each DenseLayer
+        :param num_classes: (int) number of classes for classification
+        """
+        super(DenseNet, self).__init__()
+        # first Conv2d
+        self.features = nn.Sequential(OrderedDict([
+            ("conv0", nn.Conv2d(128, num_init_features, kernel_size=7, stride=2, padding=3, bias=False)),
+            ("norm0", nn.BatchNorm2d(num_init_features)),
+            ("relu0", nn.ReLU(inplace=True)),
+            ("pool0", nn.MaxPool2d(3, stride=2, padding=1))
+        ]))
+
+        # DenseBlock
+        num_features = num_init_features
+        for i, num_layers in enumerate(block_config):
+            block = _DenseBlock(num_layers, num_features, bn_size, growth_rate, drop_rate)
+            self.features.add_module("denseblock%d" % (i + 1), block)
+            num_features += num_layers*growth_rate
+           # if i != len(block_config) - 1:
+               # transition = _Transition(num_features, int(num_features*compression_rate))
+              #  self.features.add_module("transition%d" % (i + 1), transition)
+              #  num_features = int(num_features * compression_rate)
+
+        # final bn+ReLU
+        self.features.add_module("norm5", nn.BatchNorm2d(num_features))
+        self.features.add_module("relu5", nn.ReLU(inplace=True))
+
+        # classification layer
+        #self.classifier = nn.Linear(num_features, num_classes)
+
+        # params initialization
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.bias, 0)
+                nn.init.constant_(m.weight, 1)
+            elif isinstance(m, nn.Linear):
+                nn.init.constant_(m.bias, 0)
+
+    def forward(self, x):
+        features = self.features(x)
+        #out = F.avg_pool2d(features, 7, stride=1).view(features.size(0), -1)
+        #out = self.classifier(out)
+        return features
+class ResidualBlock(nn.Module):
+    def __init__(self, dim, norm_layer, use_dropout, use_bias):
+        super(ResidualBlock, self).__init__()
+        res_block = [nn.ReflectionPad2d(1),
+                     conv_norm_relu(dim, dim, kernel_size=3, 
+                     norm_layer= norm_layer, bias=use_bias)]
+        if use_dropout:
+            res_block += [nn.Dropout(0.5)]
+        res_block += [nn.ReflectionPad2d(1),
+                      nn.Conv2d(dim, dim, kernel_size=3, padding=0, bias=use_bias),
+                      norm_layer(dim)]
+
+        self.res_block = nn.Sequential(*res_block)
+
+    def forward(self, x):
+        return x + self.res_block(x)
+
+
+def set_grad(nets, requires_grad=False):
+    for net in nets:
+        for param in net.parameters():
+            param.requires_grad = requires_grad
